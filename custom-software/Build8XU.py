8310c7dd3e38f555b0af95fe493b496bc6d66319
Robert Boerwinkle <rfboerwinkle@gmail.com>
2024-11-12 15:38:16 -0600

    reworked Build8XU.py; faster, smaller, more readable; needs cmd arguments

--- custom-software/Build8XU.py
+++ custom-software/Build8XU.py
@@ -7,56 +7,24 @@ From his readme:
 [...]
 > brandonlw@gmail.com, brandonw.net, etc.
 
-note: mine does no signing....
+NOTE: His is signed with a community key. This one requires you to provide one.
+      Assuming you're working on a TI84PSE, you should use the 0x0A key.
 """
 
 import hashlib
 import sys
 
-def appendByteArray(array):
-  global data, dataIndex
-  for char in array:
-    data[dataIndex] = char
-    dataIndex += 1
-
-def writeByteArray(f, array):
-  for num in array:
-    f.write(num.to_bytes(1, 'big'))
-
 def toIntelHexString(address, array, offset, count):
-  ret = ":"
-  ret += f"{count:0>2X}"
-  ret += f"{address:0>4X}"
-  ret += "00"
+  ret = f":{count:0>2X}{address:0>4X}00"
+  checksum = count + (address & 0xFF) + ((address >> 8) & 0xFF)
   for i in range(offset, count + offset):
     ret += f"{array[i]:0>2X}"
-  ret += f"{strToChecksum(ret):0>2X}"
-  ret += "\r\n"
+    checksum += array[i]
+    checksum &= 0xFF
+  checksum = (checksum ^ 0xFF) & 0xFF
+  checksum = (checksum + 1) & 0xFF
+  ret += f"{checksum:0>2X}\r\n"
   return ret
-
-def strToChecksum(line):
-  return arrayToCheckSum(strToArray(line))
-
-def strToArray(line):
-  ret = []
-  line = line.strip(": \r\n")
-
-  if len(line) % 2 != 0:
-    print("Invalid checksum line!")
-    quit()
-
-  length = len(line)//2
-  for i in range(length):
-    ret.append(int(line[i*2 : (i+1)*2], 16))
-  return ret
-
-def arrayToCheckSum(array):
-  ret = 0
-  for s in array:
-    ret = (ret + s) & 255
-  ret = (ret ^ 255) & 255
-  ret += 1
-  return ret & 255
 
 def Signature(inHash):
   # this should be the proper one
@@ -87,16 +55,20 @@ outputFileName = "trial.8xu"
 # PLEASE NOTE THE DIFFERENCE BETWEEN THE DIRECTORY AND THE PAGE
 pages = {0x00: (0x0000, "../src/00/base.bin"), 0x1C: (0x0000, "../src/7C/base.bin")}
 
-data = [0xFF] * ((0x7F * 0x4000) + 128)
+#                 fill    (pages * pagesize) + header...?
+data = bytearray([0xFF] * ((0x7F * 0x4000) + 128))
 dataIndex = 0
 
-appendByteArray([0x80, 0xF, 0x0, 0x0, 0x0, 0x0])
-appendByteArray([0x80, 0x11, key])
-appendByteArray([0x80, 0x21, majorVersion])
-appendByteArray([0x80, 0x31, minorVersion])
-appendByteArray([0x80, 0xA1, maxHardwareVersion])
-appendByteArray([0x80, 0x81, len(pages)])
-appendByteArray([0x80, 0x7F, 0x0, 0x0, 0x0, 0x0])
+OsHeader = b'\x80\x0F\x00\x00\x00\x00' \
+ + b'\x80\x11' + key.to_bytes(1) \
+ + b'\x80\x21' + majorVersion.to_bytes(1) \
+ + b'\x80\x31' + minorVersion.to_bytes(1) \
+ + b'\x80\xA1' + maxHardwareVersion.to_bytes(1) \
+ + b'\x80\x81' + len(pages).to_bytes(1) \
+ + b'\x80\x7F\x00\x00\x00\x00'
+OsHeaderLen = len(OsHeader)
+data[:OsHeaderLen] = OsHeader
+dataIndex += OsHeaderLen
 
 for page in pages:
   pg = page
@@ -104,33 +76,26 @@ for page in pages:
   fileName = pages[page][1]
   with open(fileName, "rb") as f:
     f.seek(offset)
-    newArray = []
-    byte = f.read(1)
-    while byte != b"":
-      newArray.append(int.from_bytes(byte, "big"))
-      byte = f.read(1)
-  start = dataIndex
-  appendByteArray(newArray)
-  dataIndex = start + 0x4000
+    newArray = f.read(0x4000)
+  data[dataIndex : dataIndex+len(newArray)] = newArray
+  dataIndex += 0x4000
 
-b = bytearray(data[:dataIndex])
-MD5Hash = hashlib.md5(b).digest()
-sig = Signature(MD5Hash)
+MD5Hash = hashlib.md5(data[:dataIndex])
+sig = Signature(MD5Hash.digest())
 
 with open(outputFileName, "wb") as f:
-  headerData = [0x2A, 0x2A, 0x54, 0x49, 0x46, 0x4C, 0x2A, 0x2A, 0x2, 0x40, 0x1, 0x88, 0x11, 0x26, 0x20, 0x7, 0x8, 0x62, 0x61, 0x73, 0x65, 0x63, 0x6F, 0x64, 0x65, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x73, 0x23, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0]
-  writeByteArray(f, headerData)
-  s = toIntelHexString(0, data, 0, 0x1B)
+  TiflHeader = b'\x2A\x2A\x54\x49\x46\x4C\x2A\x2A\x02\x40\x01\x88\x11\x26\x20\x07\x08\x62\x61\x73\x65\x63\x6F\x64\x65\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x73\x23\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+  f.write(TiflHeader)
+  s = toIntelHexString(0, data, 0, OsHeaderLen)
   f.write(s.encode(encoding="ascii"))
   s = ":00000001FF\r\n"
   f.write(s.encode(encoding="ascii"))
 
   pageHeaderBase = ":0200000200"
   pageHeader = ""
-  checksum = 0
   ptr = 0
   for page in pages:
-    pageHeader = pageHeaderBase + f"{page:0>2X}" + f"{(0xFC - page):0>2X}" + "\r\n"
+    pageHeader = pageHeaderBase + f"{page:0>2X}{(0xFC - page):0>2X}\r\n"
     f.write(pageHeader.encode(encoding="ascii"))
 
     for i in range(LINES_PER_PAGE):
@@ -138,34 +103,22 @@ with open(outputFileName, "wb") as f:
       address = i * BYTES_PER_LINE
       if page != 0:
         address = address | 0x4000
-
-      checksum = BYTES_PER_LINE
-      line = ":20" + f"{address:0>4X}" + "00"
-      checksum += address & 0xFF
-      checksum += ((address & 0xFF00) >> 8) & 0xFF
-
-      for j in range(BYTES_PER_LINE):
-        d = data[(address & 0x3FFF) + j + (ptr*0x4000) + 0x1B]
-        line += f"{d:0>2X}"
-        checksum += d
-      line += f"{((((0xFF ^ (checksum & 0xFF)) & 0xFF) + 1) & 0xFF):0>2X}\r\n"
-
+      offset = (address & 0x3FFF) + (ptr*0x4000) + OsHeaderLen
+      line = toIntelHexString(address, data, offset, BYTES_PER_LINE)
       f.write(line.encode(encoding="ascii"))
     ptr += 1
 
   s = ":00000001FF\r\n"
   f.write(s.encode(encoding="ascii"))
 
-  validationData = [0xFF] * 0x43
+  validationData = bytearray([0xFF] * 0x43)
   for i in range(64):
     validationData[i+3] = sig & 0xFF
     sig = sig >> 8
   if sig != 0:
     # Sorry, I don't really know what this code does, i'm just translating ;-;
     print("I'm not sure, but something might have gone horribly wrong with the signature...")
-  validationData[0] = 0x02
-  validationData[1] = 0x0D
-  validationData[2] = 0x40
+  validationData[:3] = b'\x02\x0D\x40'
 
   s = toIntelHexString(0, validationData, 0x00, 0x20)
   f.write(s.encode(encoding="ascii"))
